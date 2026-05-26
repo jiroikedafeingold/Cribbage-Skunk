b00f52cf9c5175092aa309e61f9600755c300b65
Jiro Feingold <jiro@feingold5.com>
2026-05-26 10:59:40 -0700

    Initial Commit

---
+++ Criboard/CriboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriboardApp.swift
+//  Criboard
+//
+//  Created by Jiro on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CriboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
